1c68b1af5a64a114e91b896abd0ec4a384b342b8
Marx, Riaan <riaan.marx@accenture.com>
2022-02-21 07:54:02 +0200

    updated

--- site generator tool/sync.cmd
+++ site generator tool/sync.cmd
@@ -1,7 +1,7 @@
 @echo off
 @SET PATH=%PATH%;C:\Program Files\IIS\Microsoft Web Deploy V3
 
-MSDEPLOY.exe -verb:sync -source:contentPath="C:\Project files\Nudibranchs\websites\Nudibranchs\js" -dest:contentPath="nudibranchs/js",ComputerName="https://nudibranchs.scm.azurewebsites.net:443/msdeploy.axd?site=Nudibranchs",authType="Basic",userName="$Nudibranchs",password="scarprfmAomg4JYr6DTz2Mp9u2wrQlDGvXj9Piqk72utSJcH0xpDmiegxSnL"
+MSDEPLOY.exe -verb:sync -source:contentPath="C:\Users\riaan.marx\Projects\Nudibranchs\websites\Nudibranchs\js" -dest:contentPath="nudibranchs/js",ComputerName="https://nudibranchs.scm.azurewebsites.net:443/msdeploy.axd?site=Nudibranchs",authType="Basic",userName="$Nudibranchs",password="scarprfmAomg4JYr6DTz2Mp9u2wrQlDGvXj9Piqk72utSJcH0xpDmiegxSnL"
 rem MSDEPLOY.exe -verb:dump
 		
 rem authType=Basic,userName=Nudibranchs\$Nudibranchs,password=scarprfmAomg4JYr6DTz2Mp9u2wrQlDGvXj9Piqk72utSJcH0xpDmiegxSnL -whatif 
